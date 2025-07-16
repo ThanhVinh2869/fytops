00cc71dbdc7d9a570c5f0ded5442b5cb79538d6d
Vincent <zukito.vincent@gmail.com>
2025-07-16 16:31:52 -0500

    Run backend

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, BOT_TOKEN
 from app.fytops import FyTops
+from app.backend import app
 
 def main():
     fytops = FyTops(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
     fytops.run(BOT_TOKEN)
+    app.run(debug=True)
     
 if __name__ == "__main__":
     main()
